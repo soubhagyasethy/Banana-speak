7d670631babbb6b670b16e8025d41076fc16c870
Soubhagya Sethy <ssethy017@gmail.com>
2022-12-26 22:38:39 +0530

    input output processing done

--- app.js
+++ app.js
@@ -8,11 +8,19 @@
 // alert(processing);
 
 let btnTranslate = document.querySelector("#btn-translate");
-let txtInput = document.querySelector("#txt-input")
-console.log(txtInput)
+let txtInput = document.querySelector("#txt-input");
+let txtOutput = document.querySelector("#txt-output");
+
 function clickEventHandler() {
-    console.log("Clicked!")
-    console.log("Input: " + txtInput.value)
+    txtOutput.innerText = txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler); // concept of callback in javascript
+
+
+// What will be the query you would write for each one below? Select the first...
+
+// ... textarea tag. document.querySelector("textarea")
+// ... element with a class btn-primary. document.queryselector(".btn-primary")
+// ... element with an id input-btn. document.querySelector("#input-btn")
+// ... input element with an attribute name='translator'. document.querySelector("input[name="translator"]")
