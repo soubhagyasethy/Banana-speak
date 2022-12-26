4c1b40637eef25c68c4a008874f141bf4e32ed36
Soubhagya Sethy <ssethy017@gmail.com>
2022-12-27 03:18:17 +0530

    added server and processing

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-// console.log("Hey Browser! Are you lonely as I'm right now? ")
-
-// // input
-// let input = prompt("Hey! What is your name? ");
-// // processing
-// let processing = "Welcome! Nice to meet you " + input;
-// // output
-// alert(processing);
 
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
 let txtOutput = document.querySelector("#txt-output");
 
-function clickEventHandler() {
-    txtOutput.innerText = txtInput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+    return serverURL + "?" +"text=" + input
 }
 
-btnTranslate.addEventListener("click", clickEventHandler); // concept of callback in javascript
+function clickEventHandler() {
+    let inputTxt = txtInput.value; //taking input
 
+    // calling server for processing
+    fetch(getTranslationURL(inputTxt)).then(response => response.json()).then(json => {
+        let translatedTxt = json.contents.translated;
+        txtOutput.innerText = translatedTxt; //output
+    })
+}
 
-// What will be the query you would write for each one below? Select the first...
+btnTranslate.addEventListener("click", clickEventHandler);
 
-// ... textarea tag. document.querySelector("textarea")
-// ... element with a class btn-primary. document.queryselector(".btn-primary")
-// ... element with an id input-btn. document.querySelector("#input-btn")
-// ... input element with an attribute name='translator'. document.querySelector("input[name="translator"]")
